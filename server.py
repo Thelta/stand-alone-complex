b85224a427558f08099cee2da455ea030a9f82df
Atılhan Dursunoğlu <atilhandursunoglu@outlook.com>
2017-05-10 02:40:19 +0300

    added create_room and show_rooms

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import select
 import socket
 import argparse
+import base64
+
+#TODO all send commands will be exceptions
 
 def create_command_parser():
     parser = argparse.ArgumentParser(prog="PROG")
@@ -12,10 +15,25 @@ def create_command_parser():
     parser_login.add_argument("password", type=str)
     parser_login.set_defaults(which="login")
 
-    parser_login = subparsers.add_parser("\\new_user")
-    parser_login.add_argument("username", type=str)
-    parser_login.add_argument("password", type=str)
-    parser_login.set_defaults(which="new_user")
+    parser_nu = subparsers.add_parser("\\new_user")
+    parser_nu.add_argument("username", type=str)
+    parser_nu.add_argument("password", type=str)
+    parser_nu.set_defaults(which="new_user")
+
+    #create subparsers for ROOM_SELECT
+    parser_join = subparsers.add_parser("\\join")
+    parser_join.add_argument("room_name", type=str)
+    parser_join.add_argument("password", nargs='?', type=str)
+    parser_join.set_defaults(which="join")
+
+    parser_showr = subparsers.add_parser("\\show_rooms")
+    parser_showr.set_defaults(which="show_rooms")
+
+    parser_create = subparsers.add_parser("\\create_room")
+    parser_create.add_argument("room_name", type=str)
+    parser_create.add_argument("password", nargs='?', type=str)
+    parser_create.add_argument("user_limit", nargs='?', type=int)
+    parser_create.set_defaults(which="create_room")
 
     return parser
 
@@ -40,7 +58,6 @@ def login_user(server_info, socket, user_info):
     else:
         socket.send(b"\\login_auth fail")
 
-    
 
 def create_new_user(server_info, socket, user_info):
     can_create = False
@@ -59,6 +76,62 @@ def create_new_user(server_info, socket, user_info):
     else:
         socket.send(b"\\create_acc_info fail")
 
+def show_rooms(server_info, socket, user_info):
+    chat_R = server_info["chat_rooms"]
+    chat_rooms_string = "Room Name \t\t Password \t Users Online\n\n"   #header
+    for room_name, room_info in zip(chat_R.keys(), chat_R.values()):
+        first_part = max(0, min(len(room_name), 10))
+        second_part = max(10, len(room_name))
+        line = room_name + " " * (10 - first_part) + "\t" * ((second_part + 8) // 8 + 1)
+
+        if room_info["password"] is None:
+            line = line + " " * 10 + "\t\t" + " "
+        else:
+            line = line + " " + "*" + " " * 8 + "\t\t" + " "
+
+        line = line + str(len(room_info["users"])) + "/{}".format(room_info["user_limit"]) + "\n"
+        chat_rooms_string += line
+    base64_string = base64.b64encode(chat_rooms_string.encode())
+    send_message = "\\list_rooms_info ok ".encode()  + base64_string
+    #base64 because it doesnt feature space.so client side parser wont have difficulty to parse.
+
+    socket.send(send_message)
+
+def create_room(server_info, socket, user_info):
+    chat_R = server_info["chat_rooms"]
+
+    room_name = user_info.room_name
+    password = user_info.password
+    user_limit = user_info.user_limit
+
+    if room_name in chat_R:
+        socket.send(b"\\create_room_info fail same_name")
+        return
+
+    if len(room_name) > 18:
+        socket.send(b"\\create_room_info fail long_name")
+        return
+
+    if not 2 < user_limit < 221:    #why 221?Because 4 is too low.
+        socket.send(b"\\create_room_info fail user_limit")
+        return
+
+    new_room = dict()
+
+    new_room["password"] = password
+    new_room["user_limit"] = user_limit
+    new_room["users"] = []
+
+    chat_R[room_name] = new_room
+
+    server_info["online_users"][socket]["room"] = room_name
+
+    socket.send(b"\\create_room_info ok")
+
+
+def join_room(server_info, socket, user_info):
+    a = 0
+
 if __name__ == "__main__":
     #socket variables
     CONN_LIST = []
@@ -67,9 +140,11 @@ if __name__ == "__main__":
 
     users = {"admin" : "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"}
     online_users = dict()
-    server_info = {"all_users": users, "online_users": online_users}
+    chat_rooms = dict()
+    server_info = {"all_users": users, "online_users": online_users, "chat_rooms": chat_rooms}
 
-    ARG_TYPES = {"LOGIN_PAGE": {"login": login_user, "new_user": create_new_user}}
+    ARG_TYPES = {"LOGIN_PAGE": {"login": login_user, "new_user": create_new_user},
+                 "ROOM_SELECT": {"join": join_room, "show_rooms": show_rooms, "create_room": create_room}}
 
     com_parser = create_command_parser()
 
@@ -97,12 +172,12 @@ if __name__ == "__main__":
                 conn_user["room"] = "LOGIN_PAGE"
 
                 online_users[sockfd] = conn_user
-
+            #TODO control user
             else:
                 try:
                     data = sock.recv(RECV_BUFFER).decode("utf-8")
                     command = data.rstrip().split(' ')
-                    command_info = com_parser.parse_args(command)
+                    command_info = com_parser.parse_known_args(command)[0]
                     if command_info.which in ARG_TYPES[online_users[sock]["room"]]:
                         ARG_TYPES[online_users[sock]["room"]][command_info.which](server_info, sock, command_info)
                 except:
